a81119a3a487ede4d66e4840c98d0006f1a120ef
hama1185 <hamataku0120@gmail.com>
2021-10-25 09:18:54 +0900

    state管理を行う

--- Assets/Scripts/SectionManager.cs
+++ Assets/Scripts/SectionManager.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum State{
+    Intro,
+    Guide,
+    Foreshadowing,
+    Reaction,
+    Fin
+}
+
 public class SectionManager : MonoBehaviour
 {
+    private State currentState;
+    
     void Start() {
         
     }
@@ -11,4 +21,42 @@ public class SectionManager : MonoBehaviour
     void Update() {
         
     }
+
+    // 他スクリプトから参照する時
+    public State CurrentState
+    {
+        get
+        {
+            return currentState;
+        }
+    }
+
+    // ステートの値を変更するとき
+    public void SetCurrentState(State state) {
+        currentState = state;
+        OnStateChanged(currentState);
+    }
+    
+    // ステートの値が変わったとき
+    void OnStateChanged(State state) {
+        switch(state) {
+            case State.Intro:
+            break;
+
+            case State.Guide:
+            break;
+
+            case State.Foreshadowing:
+            break;
+
+            case State.Reaction:
+            break;
+
+            case State.Fin:
+            break;
+
+            default:
+            break;
+        }
+    } 
 }
